e896cd7141d437fb63c80ef900e79d814fca3c6a
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-12-23 20:49:18 +0100

    Add email unique & set time stamp to true in User schema

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -1,31 +1,30 @@
 import { model, Schema } from "mongoose";
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isActive: {
+      type: Boolean,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  isActive: {
-    type: Boolean,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema, "users");
 
