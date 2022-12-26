1e60e2edca0943ed89cb7deea9dc2a2c54633807
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-12-26 12:06:27 +0100

    Add test for unknown endpoint

---
+++ src/server/app.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest";
+import app from "./app";
+import httpStatusCodes from "../utils/httpStatusCodes";
+
+const {
+  clientErrors: { notFoundCode },
+} = httpStatusCodes;
+
+describe("Given a GET /not-found endpoint", () => {
+  describe("When it receives a request", () => {
+    test("Then it should respond with status 404 and 'Unknown endpoint'", async () => {
+      const unknownEndpoint = "/not-found";
+      const expectedError = "Unknown endpoint";
+
+      const response = await request(app)
+        .get(unknownEndpoint)
+        .expect(notFoundCode);
+
+      expect(response.body).toHaveProperty("error", expectedError);
+    });
+  });
+});
