442990c8029d516fb36a832f7072581aaccb8fc9
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-12-27 20:23:14 +0100

    Validation register request with Joi

---
+++ src/server/schemas/joiTypesErrors.ts
@@ -0,0 +1,7 @@
+const joiTypesError = {
+  stringEmpty: "string.empty",
+  emailInvalid: "email.invalid",
+  stringMin: "string.min",
+};
+
+export default joiTypesError;
---
+++ src/server/schemas/registerUserSchema.ts
@@ -0,0 +1,35 @@
+import { Joi } from "express-validation";
+import type { UserStructure } from "../../database/models/User";
+import joiTypesError from "./joiTypesErrors";
+
+export interface RegisterUserBody extends UserStructure {
+  repeatedPassword: string;
+}
+
+const { emailInvalid, stringEmpty, stringMin } = joiTypesError;
+
+const registerUserSchema = {
+  body: Joi.object<RegisterUserBody>({
+    name: Joi.string()
+      .required()
+      .messages({
+        [stringEmpty]: "Name couldn't be empty",
+      }),
+    password: Joi.string()
+      .min(8)
+      .required()
+      .messages({
+        [stringMin]: "Password should have 8 characters minimum",
+        [stringEmpty]: "Password couldn't be empty",
+      }),
+    email: Joi.string()
+      .required()
+      .email()
+      .messages({
+        [stringEmpty]: "Email couldn't be empty",
+        [emailInvalid]: "Incorrect email format",
+      }),
+  }),
+};
+
+export default registerUserSchema;
