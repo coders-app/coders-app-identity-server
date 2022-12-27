2d43a648e15c11ed9b414bc61997cfec0ebfccf4
Mario González <mario@mariogl.com>
2022-12-27 06:51:22 +0100

    Create function loadJson to load dinamically a JSON file

---
+++ src/utils/loadJson.ts
@@ -0,0 +1,11 @@
+import fs from "fs";
+
+/* We can't use Node's import assertions to load JSON because ts-jest won't understand them
+/* This is a workaround from https://stackoverflow.com/a/73747606/574218 */
+/* It must be removed when Node's import assertions are stable */
+export const loadJson = <T>(path: string) => {
+  const url = new URL(path, import.meta.url);
+  const file = fs.readFileSync(url).toString();
+  const data = JSON.parse(file) as T;
+  return data;
+};
