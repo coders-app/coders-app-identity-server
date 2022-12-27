ac712fc4f3845049ab929f94cde5e53f082bab51
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-12-27 16:13:37 +0100

    Add validation error to general error middleware

--- src/server/middlewares/errors.ts
+++ src/server/middlewares/errors.ts
@@ -2,6 +2,7 @@ import "../../loadEnvironments.js";
 import debugCreator from "debug";
 import chalk from "chalk";
 import type { NextFunction, Request, Response } from "express";
+import { ValidationError } from "express-validation";
 import CustomError from "../../CustomError/CustomError.js";
 import httpStatusCodes from "../../utils/httpStatusCodes.js";
 
@@ -19,11 +20,21 @@ const generalError = (
   // eslint-disable-next-line no-unused-vars
   next: NextFunction
 ) => {
-  debug(chalk.bold.red(error.message));
+  if (error instanceof ValidationError) {
+    const validationErrors = error.details.body
+      .map((joiError) => joiError.message)
+      .join("\n");
+
+    error.publicMessage = validationErrors;
+  }
+
+  debug(chalk.red(error));
 
   const statusCode = error.statusCode || internalServerErrorCode;
   const publicMessage =
     error.publicMessage || "There was an error on the server";
+
+  debug(chalk.bold.red(error.message));
 
   res.status(statusCode).json({ error: publicMessage });
 };
