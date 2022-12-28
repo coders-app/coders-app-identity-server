3dc5a89f2f32ac51cb380658e77dc2167ab7a3ee
Berto Forteza <bertoforteza87@gmail.com>
2022-12-28 18:21:26 +0100

    Add optional params to getMockUser function

--- src/factories/usersFactory.ts
+++ src/factories/usersFactory.ts
@@ -2,13 +2,14 @@ import { Factory } from "fishery";
 import type { UserStructure } from "../database/models/User";
 import { faker } from "@faker-js/faker";
 
-const userFactory = Factory.define<UserStructure>(() => ({
-  name: faker.internet.userName(),
-  email: faker.internet.email(),
-  password: faker.internet.password(10),
+const userFactory = Factory.define<UserStructure>(({ params }) => ({
+  name: faker.internet.userName() || params.name,
+  email: faker.internet.email() || params.email,
+  password: faker.internet.password(10) || params.password,
   isActive: false,
   createdAt: faker.date.past(),
   updatedAt: faker.date.recent(),
 }));
 
-export const getMockUser = () => userFactory.build();
+export const getMockUser = (params?: Partial<UserStructure>) =>
+  userFactory.build(params);
