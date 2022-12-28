32b7548b4e66f2b8eb56d7a97b7742db648e52d2
Berto Forteza <bertoforteza87@gmail.com>
2022-12-28 09:29:46 +0100

    Create userFactory and getRandomUser function

---
+++ src/factories/usersFactory.ts
@@ -0,0 +1,14 @@
+import { Factory } from "fishery";
+import type { UserStructure } from "../database/models/User";
+import { faker } from "@faker-js/faker";
+
+const userFactory = Factory.define<UserStructure>(() => ({
+  name: faker.internet.userName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(10),
+  isActive: false,
+  createdAt: faker.date.past(),
+  updatedAt: faker.date.recent(),
+}));
+
+export const getRandomUser = () => userFactory.build();
