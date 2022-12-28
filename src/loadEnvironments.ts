2a91377a84d3d94069f9fffe351e2386a3505916
Mario González <mario@mariogl.com>
2022-12-28 13:53:32 +0100

    Add Basic Auth username&password to environment variables

--- src/loadEnvironments.ts
+++ src/loadEnvironments.ts
@@ -7,6 +7,8 @@ const {
   MONGODB_URL: mongoDbUrl,
   MONGODB_DEBUG: mongoDbDebug,
   ORIGIN_WHITELIST: originWhitelist,
+  BASIC_AUTH_USERNAME: basicAuthUsername,
+  BASIC_AUTH_PASSWORD: basicAuthPassword,
 } = process.env;
 
 export const environment = {
@@ -14,4 +16,8 @@ export const environment = {
   mongoDbUrl,
   mongoDbDebug: mongoDbDebug === "true",
   originWhitelist: originWhitelist.split(","),
+  basicAuth: {
+    username: basicAuthUsername,
+    password: basicAuthPassword,
+  },
 };
