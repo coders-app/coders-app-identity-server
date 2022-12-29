6a3b500e8c00ebc5588b1b00c19d007721c85ab7
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-12-29 19:59:03 +0100

    Add openApiConfigFile to index openapi

--- src/openapi/index.ts
+++ src/openapi/index.ts
@@ -5,6 +5,7 @@ import { loadJson } from "../utils/loadJson.js";
 const __dirname = fileURLToPath(path.dirname(import.meta.url));
 const openApiConfigFile = path.join(__dirname, "openapi.json");
 
+console.log(openApiConfigFile);
 const openApiDocument = loadJson<Record<string, unknown>>(openApiConfigFile);
 
 export default openApiDocument;
