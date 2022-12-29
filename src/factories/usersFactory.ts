c68fc6236030a30b42dfbd7085cb190e76242c83
Berto Forteza <bertoforteza87@gmail.com>
2022-12-29 16:58:05 +0100

    Create registerUserFactory and getMockRegisterUser function

--- src/factories/usersFactory.ts
+++ src/factories/usersFactory.ts
@@ -1,6 +1,7 @@
 import { Factory } from "fishery";
 import type { UserStructure } from "../database/models/User";
 import { faker } from "@faker-js/faker";
+import type { RegisterUserBody } from "../server/schemas/registerUserSchema";
 
 const userFactory = Factory.define<UserStructure>(() => ({
   name: faker.internet.userName(),
@@ -11,5 +12,14 @@ const userFactory = Factory.define<UserStructure>(() => ({
   updatedAt: faker.date.recent(),
 }));
 
+const registerUserFactory = Factory.define<Partial<RegisterUserBody>>(() => ({
+  name: faker.internet.userName(),
+  password: faker.internet.password(10),
+  email: faker.internet.email(),
+}));
+
 export const getMockUser = (params?: Partial<UserStructure>) =>
   userFactory.build(params);
+
+export const getMockRegisterUser = (params?: Partial<RegisterUserBody>) =>
+  registerUserFactory.build(params);
