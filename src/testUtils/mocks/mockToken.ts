73f2fbd6cb526bb7edc541feb6f18cb1afe4b961
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-12-29 10:43:00 +0100

    Test userAuthentication controller

---
+++ src/testUtils/mocks/mockToken.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import { environment } from "../../loadEnvironments.js";
+import type { CustomTokenPayload } from "../../server/controllers/userControllers/types";
+
+const { jwtSecret } = environment;
+
+export const mockTokenPayload: CustomTokenPayload = {
+  name: "admin",
+  id: "637ca68b2e7c24060c5c7e20",
+};
+
+const token = jwt.sign(mockTokenPayload, jwtSecret);
+
+export const mockToken = `Bearer ${token}`;
