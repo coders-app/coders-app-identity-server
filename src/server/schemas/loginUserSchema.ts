741d32564d8705c85aadf6fe5c01cdf32300554d
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-12-29 16:49:34 +0100

    Extract password and email schemas to userCredentialsSchemas

--- src/server/schemas/loginUserSchema.ts
+++ src/server/schemas/loginUserSchema.ts
@@ -1,6 +1,6 @@
 import { Joi } from "express-validation";
 import type { LoginCredentials } from "../controllers/userControllers/types";
-import { emailSchema, passwordSchema } from "./registerUserSchema.js";
+import { emailSchema, passwordSchema } from "./userCredentialSchemas.js";
 
 const loginUserSchema = {
   body: Joi.object<LoginCredentials>({
--- src/server/schemas/registerUserSchema.ts
+++ src/server/schemas/registerUserSchema.ts
@@ -1,29 +1,13 @@
 import { Joi } from "express-validation";
 import type { UserStructure } from "../../database/models/User";
 import joiTypesError from "./joiTypesErrors.js";
+import { emailSchema, passwordSchema } from "./userCredentialSchemas.js";
 
 export interface RegisterUserBody extends UserStructure {
   repeatedPassword: string;
 }
 
-const { emailInvalid, stringEmpty, stringMin } = joiTypesError;
-
-export const emailSchema = Joi.string()
-  .required()
-  .email()
-  .messages({
-    [stringEmpty]: "Email shouldn't be empty",
-    [emailInvalid]: "Incorrect email format",
-  })
-  .label("Email");
-
-export const passwordSchema = Joi.string()
-  .min(8)
-  .required()
-  .messages({
-    [stringMin]: "Password should have 8 characters minimum",
-    [stringEmpty]: "Password shouldn't be empty",
-  });
+const { stringEmpty } = joiTypesError;
 
 const registerUserSchema = {
   body: Joi.object<RegisterUserBody>({
