5d44bef24ce236e2da2fc9358c1751666e2b45b4
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2023-01-03 18:02:04 +0100

    Create send mail function

--- src/server/email/transporter.ts
+++ src/server/email/transporter.ts
@@ -1,14 +1,14 @@
 import nodemailer from "nodemailer";
-import { environment } from "../../loadEnvironments";
+import { environment } from "../../loadEnvironments.js";
 
-const { smtp, sendgridApiKey } = environment;
+const { smtp } = environment;
 
 const transporter = nodemailer.createTransport({
   host: smtp.host,
   port: smtp.port,
   auth: {
     user: smtp.username,
-    pass: sendgridApiKey,
+    pass: smtp.password,
   },
 });
 
