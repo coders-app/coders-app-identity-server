518cf66a16324e2fa088a6fd8d3236c658e75b3b
Liluminalia <peloazul4@gmail.com>
2023-01-09 17:39:13 +0100

    updating error messages in app test file

--- src/server/app.test.ts
+++ src/server/app.test.ts
@@ -1,15 +1,21 @@
 import request from "supertest";
 import app from "./app";
 import httpStatusCodes from "../utils/httpStatusCodes";
+import httpErrorMessages from "../utils/httpErrorMessages";
+import publicHttpErrorMessages from "../utils/publicHttpErrorMessages";
 
 const {
   clientErrors: { badRequestCode, notFoundCode },
 } = httpStatusCodes;
-
+const {
+  clientErrors: { unknownEndpointMsg },
+} = httpErrorMessages;
+const {
+  publicClientErrors: { publicBadRequestMsg },
+} = publicHttpErrorMessages;
 describe("Given a GET / endpoint", () => {
   describe("When it receives a request from an origin that is not whitelisted", () => {
     test("Then it should respond with status 400 and the message 'Blocked by CORS'", async () => {
-      const expectedMessage = "Not allowed by CORS";
       const unknownOrigin = "http://localhost:1234";
 
       const response = await request(app)
@@ -17,7 +23,10 @@ describe("Given a GET / endpoint", () => {
         .set("origin", unknownOrigin)
         .expect(badRequestCode);
 
-      expect(response.body).toHaveProperty("error", expectedMessage);
+      expect(response.body).toHaveProperty(
+        "error",
+        unknownOrigin + publicBadRequestMsg
+      );
     });
   });
 });
@@ -26,13 +35,12 @@ describe("Given a GET /not-found endpoint", () => {
   describe("When it receives a request", () => {
     test("Then it should respond with status 404 and 'Unknown endpoint'", async () => {
       const unknownEndpoint = "/not-found";
-      const expectedError = "Unknown endpoint";
 
       const response = await request(app)
         .get(unknownEndpoint)
         .expect(notFoundCode);
 
-      expect(response.body).toHaveProperty("error", expectedError);
+      expect(response.body).toHaveProperty("error", unknownEndpointMsg);
     });
   });
 });
