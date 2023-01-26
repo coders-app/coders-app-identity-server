66551106142508012237f0f833ba648d8513c11f
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2023-01-26 16:12:19 +0100

    Add cookie-parser middleware to verifyTokenRouter & update router tests

--- src/server/routers/verifyTokenRouter/verifyTokenRouter.ts
+++ src/server/routers/verifyTokenRouter/verifyTokenRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
+import cookieParser from "cookie-parser";
 import userAuthentication from "../../controllers/authControllers/authControllers.js";
 import paths from "../paths.js";
-
 const { verifyToken } = paths;
 
 // eslint-disable-next-line new-cap
 const verifyTokenRouter = Router();
+
+verifyTokenRouter.use(cookieParser());
 
 verifyTokenRouter.get(verifyToken, userAuthentication);
 
