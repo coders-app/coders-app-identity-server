76530f9e57ba140a2dae6bf04f4f7e4f19878d97
Mario González <mario@mariogl.com>
2023-02-19 08:18:24 +0100

    Move constants to their folder

---
+++ src/constants/statusCodes/httpStatusCodes.ts
@@ -0,0 +1,19 @@
+const httpStatusCodes = {
+  successCodes: {
+    okCode: 200,
+    createdCode: 201,
+  },
+
+  clientErrors: {
+    badRequestCode: 400,
+    unauthorizedCode: 401,
+    notFoundCode: 404,
+    conflictCode: 409,
+  },
+
+  serverErrors: {
+    internalServerErrorCode: 500,
+  },
+};
+
+export default httpStatusCodes;
