fe390f934823d830cbd51c11b6e2302ff0246bed
Lorena Martínez Moledo <105820543+MMLORENA@users.noreply.github.com>
2023-02-28 09:23:16 +0100

    Revert "Feature/cod 81 authenitcate api key in requests"

--- src/constants/appAuthenticationNames.ts
+++
@@ -1,9 +0,0 @@
-import { environment } from "../loadEnvironments";
-
-const appAuthenticationNames = {
-  apiKeyHeader: "X-API-KEY",
-  current: environment.appName,
-  apiGateway: "api-gateway",
-};
-
-export default appAuthenticationNames;
