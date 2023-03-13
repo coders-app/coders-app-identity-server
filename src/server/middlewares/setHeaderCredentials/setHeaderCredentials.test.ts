52d7f179a4ae87cbd5dddd3ee24be32ad63b4d05
Lorena Martinez Moledo <lmartmol@gmail.com>
2023-03-13 10:35:34 +0100

    Separate setHeaderCredentials in 2 expects

--- src/server/middlewares/setHeaderCredentials/setHeaderCredentials.test.ts
+++ src/server/middlewares/setHeaderCredentials/setHeaderCredentials.test.ts
@@ -1,22 +1,31 @@
 import type { Request, Response, NextFunction } from "express";
+import requestHeaders from "../../../constants/requestHeaders";
 import setHeaderCredentials from "./setHeaderCredentials";
 
+const { allowCredentials } = requestHeaders;
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe("Given a setHeaderCredentials middleware", () => {
-  describe("When it receives a request, response and next function", () => {
-    test("Then it should invoke response's method header with 'Access-Control-Allow-Credentials', 'true' and next function", () => {
-      const req: Partial<Request> = {};
-      const res: Partial<Response> = {
-        header: jest.fn(),
-      };
+  const req: Partial<Request> = {};
+  const res: Partial<Response> = {
+    header: jest.fn(),
+  };
 
-      const next: NextFunction = jest.fn();
+  const next: NextFunction = jest.fn();
 
+  describe("When it receives a response and next function", () => {
+    test("Then it should invoke response's method header with 'Access-Control-Allow-Credentials', 'true'", () => {
       setHeaderCredentials(req as Request, res as Response, next);
 
-      expect(res.header).toHaveBeenCalledWith(
-        "Access-Control-Allow-Credentials",
-        "true"
-      );
+      expect(res.header).toHaveBeenCalledWith(allowCredentials, "true");
+    });
+
+    test("Then it should invoke next function", () => {
+      setHeaderCredentials(req as Request, res as Response, next);
+
       expect(next).toHaveBeenCalled();
     });
   });
